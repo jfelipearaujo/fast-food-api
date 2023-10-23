afa48632d455d2144aa22076f44a848925c10b57
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2023-10-23 15:55:52 -0300

    feat: adicinado seed scripts

--- Makefile
+++ Makefile
@@ -1,9 +1,28 @@
 .PHONY:
 	up \
 	down \
+	seed-all \
+	seed-lanches \
+	seed-acompanhamentos \
+	seed-bebidas \
+	seed-sobremesas \
 
 up:
 	docker compose -f ./api/CtrlEat/docker-compose.yml up -d
 
 down:
 	docker compose -f ./api/CtrlEat/docker-compose.yml down
+
+seed-all: seed-lanches seed-acompanhamentos seed-bebidas seed-sobremesas
+
+seed-lanches:
+	sh ./api/CtrlEat/scripts/api/seed_lanches.sh
+
+seed-acompanhamentos:
+	sh ./api/CtrlEat/scripts/api/seed_acompanhamentos.sh
+
+seed-bebidas:
+	sh ./api/CtrlEat/scripts/api/seed_bebidas.sh
+
+seed-sobremesas:
+	sh ./api/CtrlEat/scripts/api/seed_sobremesas.sh
---
+++ api/CtrlEat/scripts/api/seed_acompanhamentos.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+# seed product categories
+echo "Acompanhamentos - Seeding product categories..."
+
+categoryId=$(curl -X 'POST' \
+  'http://localhost:5001/api/v1/products/categories' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "description": "Acompanhamentos"
+}' 2>/dev/null | jq --raw-output '"\(.id)"')
+
+echo "Acompanhamentos - Seeding products..."
+
+productId_1=$(curl -X 'POST' \
+  'http://localhost:5001/api/v1/products' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "productCategoryId": "'"$categoryId"'",
+  "description": "Batata Frita Crocante",
+  "amount": 3.99,
+  "currency": "BRL",
+  "ImageUrl": ""
+}' 2>/dev/null | jq --raw-output '"\(.id)"')
+
+productId_2=$(curl -X 'POST' \
+  'http://localhost:5001/api/v1/products' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "productCategoryId": "'"$categoryId"'",
+  "description": "Anéis de Cebola Fritos",
+  "amount": 4.5,
+  "currency": "BRL",
+  "ImageUrl": ""
+}' 2>/dev/null | jq --raw-output '"\(.id)"')
+
+productId_3=$(curl -X 'POST' \
+  'http://localhost:5001/api/v1/products' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "productCategoryId": "'"$categoryId"'",
+  "description": "Nachos com Queijo e Guacamole",
+  "amount": 5.25,
+  "currency": "BRL",
+  "ImageUrl": ""
+}' 2>/dev/null | jq --raw-output '"\(.id)"')
+
+productId_4=$(curl -X 'POST' \
+  'http://localhost:5001/api/v1/products' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "productCategoryId": "'"$categoryId"'",
+  "description": "Molho de Queijo com Bacon",
+  "amount": 4.75,
+  "currency": "BRL",
+  "ImageUrl": ""
+}' 2>/dev/null | jq --raw-output '"\(.id)"')
+
+productId_5=$(curl -X 'POST' \
+  'http://localhost:5001/api/v1/products' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "productCategoryId": "'"$categoryId"'",
+  "description": "Salada de Repolho Coleslaw",
+  "amount": 2.25,
+  "currency": "BRL",
+  "ImageUrl": ""
+}' 2>/dev/null | jq --raw-output '"\(.id)"')
+
+echo "Acompanhamentos - Seeding product images..."
+
+curl -X "POST" \
+  "http://localhost:5001/api/v1/products/"$productId_1"/image" \
+  -H "accept: */*" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@./api/CtrlEat/scripts/api/images/acompanhamentos/product_01.jpg;type=image/jpeg" 2>/dev/null
+
+curl -X "POST" \
+  "http://localhost:5001/api/v1/products/"$productId_2"/image" \
+  -H "accept: */*" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@./api/CtrlEat/scripts/api/images/acompanhamentos/product_02.jpg;type=image/jpeg" 2>/dev/null
+
+curl -X "POST" \
+  "http://localhost:5001/api/v1/products/"$productId_3"/image" \
+  -H "accept: */*" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@./api/CtrlEat/scripts/api/images/acompanhamentos/product_03.jpg;type=image/jpeg" 2>/dev/null
+
+curl -X "POST" \
+  "http://localhost:5001/api/v1/products/"$productId_4"/image" \
+  -H "accept: */*" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@./api/CtrlEat/scripts/api/images/acompanhamentos/product_04.jpg;type=image/jpeg" 2>/dev/null
+
+curl -X "POST" \
+  "http://localhost:5001/api/v1/products/"$productId_5"/image" \
+  -H "accept: */*" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@./api/CtrlEat/scripts/api/images/acompanhamentos/product_05.jpg;type=image/jpeg" 2>/dev/null
+
+echo "Acompanhamentos - Done"
