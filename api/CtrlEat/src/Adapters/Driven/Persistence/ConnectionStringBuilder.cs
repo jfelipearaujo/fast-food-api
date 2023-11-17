00712dac31abbe0391c818d4cdc4c85ec99331f7
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-11-17 17:17:18 -0300

    feat: ajustado a configuração do DB via env var

---
+++ api/CtrlEat/src/Adapters/Driven/Persistence/ConnectionStringBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Adapters.Database;
+
+using Npgsql;
+
+namespace Persistence;
+
+public class ConnectionStringBuilder : IConnectionStringBuilder
+{
+    private readonly string? connectionString;
+
+    public ConnectionStringBuilder()
+    {
+    }
+
+    public ConnectionStringBuilder(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public string Build()
+    {
+        if (connectionString is not null)
+        {
+            return connectionString;
+        }
+
+        var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+        {
+            Database = Environment.GetEnvironmentVariable("DB_NAME"),
+            Host = Environment.GetEnvironmentVariable("DB_HOST"),
+            Port = int.Parse(Environment.GetEnvironmentVariable("DB_PORT")),
+            Username = Environment.GetEnvironmentVariable("DB_USER"),
+            Password = Environment.GetEnvironmentVariable("DB_PASS")
+        };
+
+        return connectionStringBuilder.ConnectionString;
+    }
+}
--- api/CtrlEat/src/Adapters/Driven/Persistence/PersistenceDependency.cs
+++ api/CtrlEat/src/Adapters/Driven/Persistence/PersistenceDependency.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Domain.Adapters.Database;
+
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Persistence;
 
 public static class PersistenceDependency
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
     {
-        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("AppDbCtrlEat")));
+        services.AddSingleton<IConnectionStringBuilder, ConnectionStringBuilder>();
+
+        // ---
+
+        using var scope = services.BuildServiceProvider().CreateScope();
+
+        var connectionStringBuilder = scope.ServiceProvider.GetRequiredService<IConnectionStringBuilder>();
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(connectionStringBuilder.Build()));
 
         return services;
     }
